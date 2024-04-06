3bcb76d013c2ef79c893415acc3fa68b667f8a75
sadbrain <riotgheptrannhulol@gmail.com>
2024-04-06 18:15:00 +0700

    review rest

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-// const logger = function(log){
-//     console.log(log);
-// }
-// function logger(log){
-//     console.log(log);
-// }
-//arrow function
+//destructuring phan ra mot obj
 
-const logger = (log) => console.log(log);
-logger("NT");
+var array = ['JS', 'JAVA', 'C#']
 
-const course = {
-    name: "Javascrip basic",
-    getName: () => {
-        return this;
-    }
-    
+var [a,,c] = array;
+console.log("1. destructuring")
+console.log(a,c)
+
+// output JS C#
+
+//rest la nhung phan tu con lai va khi no dung ket hop voi
+//destructuring
+var [a, ...c] = array;
+console.log("2. rest")
+console.log(a, c)
+//output JS , ['JAVA', 'C#']
+
+
+//xample with object
+var course ={
+    name: "JS",
+    price: 1000,
+    image: "default"
 }
-console.log(course.getName());
+var {name, ...rest} = course;
+console.log("3. rest with object")
+console.log(name, rest);
+
+//out put JS { price: 1000,  image: "default"}
+
+//if obj has child obj
+console.log("4. rest with object has child object")
+var course ={
+    name: "JS",
+    price: 1000,
+    image: "default",
+    children: {
+        name: "C#"
+    }
+}
+var {name :parentName, children:{name}, description = "default description"} = course;
+console.log(parentName, name, description);
