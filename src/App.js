ceb8c0d25d352b995af4d74460ae49731af0a206
Sadbrain <riotgheptrannhulol@gmail.com>
2024-04-26 20:43:39 +0700

    feature: use useEffect call api

--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
-import {useState} from "react";
-//How to use useState
-// const Component = () => {
-//   //initState gia tri khoi tao chi dung mot lan de khoi tao gia tri dau
-//   //first state = initState
-//   //setState => function to set state => state se la gia tri cua giu lieu
-//   const [state, setState] = useState(initState);
-// }
+import Content from "./Content"
+import { useEffect , useState} from "react";
 
-//bai tap tang so 
-function App() {
-  const [counter, setCounter] = useState(1);
-  const handleIncrease = () => {
-        setCounter(counter + 1);
-  }
+//necessary knowledge 
+//events: Add/ remove event listener
+//observer pattern: Subscribe/ Unsubscribe
+//Closure
+//timers: setInterval, setTimeout, clearInterval, clearTimeout
+// useState
+// Mounted/ Unmounted 
+// ====
+//Call API
+
+
+//
+// 1. Update DOM
+// 2. call API 
+// 3. listen DOM envets
+    // - scroll 
+    // - resize
+// 4. clean up 
+    // - remove listener/ unsubcribe 
+    // - clear timer
+const App = () => {
+  // 1. useEffect(callback)
+    // - call callback every time the component re-render
+    // - call callback after component is added to DOM
+  // 1.3 code in callback 
+  // 2. useEffect(callback, [])
+    // - only call callback after component is mounted
+    // => when component re-render, callback will not be called
+  // 3. useEffect(callback, [deps])
+
+  //--------, general theory
+  //callback is always called after component is mounted
+  // 1. 
+  const [title, setTitle] = useState('')
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    console.log("re-render")
+    document.title = title
+  })
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/posts")
+  //   .then(res => res.json())
+  //   .then(posts => {
+  //     setPosts(posts);
+  //   })
+  // }, [])
   return (
-    <div className="App">
-        <h1>
-          {counter}
-        </h1>
-        <button onClick={handleIncrease} style={{padding:20, borderRadius:20, fontSize: 30}}>
-           increase
-        </button>
+    <div style={{padding: 30}}>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+       {/* <ul>
+          {posts.map((p, i) => (
+            <li key={i}>{p.id + " " +p.title}</li>
+          ))}     
+       </ul>         */}
     </div>
   )
-  //khi click on button call App function again
-
-  //click 1 lan se tang lenb ba lan ma khong phai + 3 => useState with callback
-//   const handleIncrease = () => {
-//     setCounter(preState => preState+1);
-//     setCounter(preState => preState+1);
-//     setCounter(preState => preState+1);
-// }
-// initState with call back 
-
 }
-const orders = [100 ,200, 300];
+
 
 export default App;
