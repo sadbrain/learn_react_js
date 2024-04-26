2eed8f7b3ef07358aacd66955bde9cb244b71239
sadbrain <riotgheptrannhulol@gmail.com>
2024-04-26 21:41:21 +0700

    as

--- src/App.js
+++ src/App.js
@@ -1,96 +1,11 @@
 import Content from "./Content"
-import { useEffect , useState} from "react";
-
-//necessary knowledge 
-//events: Add/ remove event listener
-//observer pattern: Subscribe/ Unsubscribe
-//Closure
-//timers: setInterval, setTimeout, clearInterval, clearTimeout
-// useState
-// Mounted/ Unmounted 
-// ====
-//Call API
-
-
-//
-// 1. Update DOM
-// 2. call API 
-// 3. listen DOM envets
-    // - scroll 
-    // - resize
-// 4. clean up 
-    // - remove listener/ unsubcribe 
-    // - clear timer
-
-
-//        call api with dependencies
-const tabs = ["posts", "comments", "albums"]
+//dem nguoc tu 180 ve 0
 const App = () => {
-  // 1. useEffect(callback)
-    // - call callback every time the component re-render
-    // - call callback after component is added to DOM
-  // 1.3 code in callback 
-  // 2. useEffect(callback, [])
-    // - only call callback after component is mounted
-    // => when component re-render, callback will not be called
-  // 3. useEffect(callback, [deps])
-
-  //--------, general theory
-  //callback is always called after component is mounted
-  // 1. --call back duoc goi lai dependencies bi thay doi
-  //2. clean up luon duoc goi khi component bi unmount
-  const [posts, setPosts] = useState([])
-  const [type, setType] = useState('posts')
-  const [showGoToTop, SetShowGoToTop] = useState(false)
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/${type}")
-    .then(res => res.json())
-    .then(posts => {
-      setPosts(posts);
-    })
-  }, [type])
-
-  useEffect(() => {
-    const handleScroll = () => {
-        SetShowGoToTop(window.scrollY >= 200)
-    }
-    window.addEventListener('scroll', handleScroll)
-
-    return()=>{
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
-  //dependencies is simplely variable
+  
   return (
-
-    <div style={{padding: 30}}>
-        {tabs.map((tab, i) => (
-          <button key={i}
-            style={type === tab ? {
-              color:'#fff',
-              backgroundColor:'#333'
-            } : {}}
-            onClick={() => setType(tab)}
-          >
-            {tab}
-          </button>
-        ))}
-
-        {posts.map((post,i) => (
-          <li key={i}>{post.title || post.name}</li>
-        ))}
-
-        {showGoToTop && (
-          <button style={{
-            position: "fixed",
-            right:20,
-            bottom:20
-          }}>
-            Go to top
-          </button>
-        )}
-    </div>
-
+    <>
+      <Content />
+    </>
   )
 }
 
--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,17 @@
+import { useState, useEffect } from "react";
+
 function Content() {
+  const [countDown, SetCountDown] = useState(180);
+    useEffect(() => {
+        const timerId = setInterval(()=>{
+            SetCountDown(prev => prev - 1)
+        }, 1000)
+
+        return () => clearInterval(timerId);
+     })
     return (
-        <h1>Hello babe
+        <h1>
+            {countDown}
         </h1>
     )
 }
