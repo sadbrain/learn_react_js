59195d3537b9416b91df3a4a6c0806c851658e27
sadbrain <riotgheptrannhulol@gmail.com>
2024-04-27 17:16:31 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,34 +1,56 @@
 import Content from "./Content"
-import { useState, Memo, useCallback } from "react";
-// Use Memo
-//1. memo() => higher order component  (HOC)
-// 2. useCallback => tranh tao ra cac ham moi khong can thiet trong function component
-  // - Reference type
-  // - React memo()
+import { useState, useMemo, useCallback, useRef } from "react";
+// mome trang prop of a component render lai khong can thiet 
+//useMemo thi tranh lap lai logic khong can thiet
 
 const App = () => {
-  const [count, setCount] = useState(60)
-  const handleIncrease = useCallback(() => {
-      setCount(prev => prev + 1)
-  }, [])
+  const [products, setProducts] = useState([])
+  const [price, setPrice] = useState("")
+  const [name, setName] = useState("")
+  const nameInRef = useRef()
+  const handleSumbit = () => {
+    setProducts([...products, {
+      name,
+      price: +price
+    }])
+
+    setName("")
+    setPrice("")
+
+    nameInRef.current.focus()
+  }
+  // const total = reduce no se bi tinh lai mot cach khong can thiet 
+  // => useMemo 
+  const total = useMemo(() => {
+    console.log("Tinh toan lai")
+    const result = products.reduce((result, prod) => {
+        return result + prod.price
+    }, 0)
+
+    return result
+  }, [products]) //=> giong voi denpen of useEffect
+  console.log(products)
   return (
 
-    // xu ly logic bat dau bang handle 
-    //cho logic xay ra bat dau bang on
-    //example: handleIncrease onIncrease
-    //bo cai nut qua Content Component thi no van se bi re-render
-    //vi mot khi App component render lai no se tao ra mot ham moi la handleIncrease
-    //nen khi gan lai cho Content component thi thang memo se thay hai thang nay khong bang nhau do no van luu cai ham cu
-    //suy ra thi lam sao cho ham ma memo ghi nho va ham duoc tao ra trong App component phai cung tham chieu toi mot ham
-    // => useCallback 
       <div>
-        <Content onIncrease={handleIncrease}/>
-        {/* content bi render lai mat du khong can thiet vay ta phai lam the nao 
-          use memo cho contenct qua content component
-         */}
-          {
-              count
-          }
+          <input
+            ref={nameInRef}
+            value={name}
+            placeholder="Enter name..."
+            onChange={e => setName(e.target.value)}
+          />
+            <input
+            value={price}
+            placeholder="Enter price..."
+            onChange={e => setPrice(e.target.value)}
+          />
+          <button onClick={handleSumbit}>sumbit</button>
+        Total: {total}
+        <ul>
+          {products.map((p, i) => {
+            <li key={i}>{p.name} - {p.price}</li>
+          })}
+        </ul>
       </div>
   )
 }
