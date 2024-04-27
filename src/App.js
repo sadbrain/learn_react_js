afcb1f1f67b42bf070804370bc2990e96ad9ae1f
sadbrain <riotgheptrannhulol@gmail.com>
2024-04-27 16:58:11 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import Content from "./Content"
-import { useState, Memo } from "react";
+import { useState, Memo, useCallback } from "react";
 // Use Memo
 //1. memo() => higher order component  (HOC)
-// 2. useCallback 
+// 2. useCallback => tranh tao ra cac ham moi khong can thiet trong function component
+  // - Reference type
+  // - React memo()
 
-// Hooks => moc vao gan vao => use in function component
-// HOC => grap component
-// Render props 
-// ====> extends login agian 
-
-// memo ghi nho lai cac prop cua a component de quyet dinh co render lai component do khong 
-//toi uu hieu nang
 const App = () => {
   const [count, setCount] = useState(60)
-  const increase = () => {
+  const handleIncrease = useCallback(() => {
       setCount(prev => prev + 1)
-  }
+  }, [])
   return (
+
+    // xu ly logic bat dau bang handle 
+    //cho logic xay ra bat dau bang on
+    //example: handleIncrease onIncrease
+    //bo cai nut qua Content Component thi no van se bi re-render
+    //vi mot khi App component render lai no se tao ra mot ham moi la handleIncrease
+    //nen khi gan lai cho Content component thi thang memo se thay hai thang nay khong bang nhau do no van luu cai ham cu
+    //suy ra thi lam sao cho ham ma memo ghi nho va ham duoc tao ra trong App component phai cung tham chieu toi mot ham
+    // => useCallback 
       <div>
-        <Content/>
+        <Content onIncrease={handleIncrease}/>
         {/* content bi render lai mat du khong can thiet vay ta phai lam the nao 
           use memo cho contenct qua content component
          */}
           {
               count
           }
-          <button onClick={increase}>Increase</button>
       </div>
   )
 }
--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,12 @@
 import { memo } from "react";
-function Content() {
+function Content({onIncrease}) {
     console.log("re-render")
-    return(<h1>hi mama</h1>)
+    return(
+            <>
+                <h1>hi mama</h1>
+                <button onClick={onIncrease}>Increase</button>
+            </>
+         )
 
 }
 export default memo(Content);
