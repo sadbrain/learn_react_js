f8f67ff6c0be26b6042f31e216767e2900d72020
sadbrain <riotgheptrannhulol@gmail.com>
2024-04-27 20:11:51 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useReducer } from "react";
+import { useState, useReducer, useRef } from "react";
 
 // use Reducer 
 //cho them mot su lua chon de su dung state cho function componet
@@ -8,43 +8,105 @@ import { useState, useReducer } from "react";
 //use reducer dung cho nhung stat phuc tap hon nhu array, obj long nhau
 
 
-//use Reducer
+//use Reducer with to do app
 // 1. init state 
 // 2. Action 
 // 3. reducer 
 // 4. dispatch 
+
 // 1. 
-const initState = 0;
+const initState = {
+  job: "",
+  jobs: []
+}
 // 2. 
-const UP_ACTION = "up";
-const DOWN_ACTION = "down";
-//3.
-const reducer = (state, action) =>{
-  switch(action){
-      case UP_ACTION:
-        return state + 1
-      case DOWN_ACTION:
-        return state - 1
-      default:
-        throw new Error("Invalid action")
+const SET_JOB = 'set_job'
+const ADD_JOB = 'add_job'
+const DELETE_JOB = 'delete_job'
+
+//du lieu am nguoi dung mang theo di
+const setJob = payload => {
+  return {
+    type: SET_JOB,
+    payload
   }
 }
+const addJob = payload => {
+  return {
+    type: ADD_JOB,
+    payload
+  }
+}
+const deleteJob = payload => {
+  return {
+    type: DELETE_JOB,
+    payload
+  }
+}
+//3. 
+const reducer = (state, action) =>{
+  let newState;
+  switch(action.type){
+    case SET_JOB:
+      newState = {
+        ...state,
+        job: action.payload
+      }
+      break
+      case ADD_JOB:
+        newState = {
+          ...state,
+          jobs: [...state.jobs, action.payload.job]
+        }
+        break
+        case DELETE_JOB:
+          const newJobs = [...state.jobs]
+          newJobs.splice(action.payload, 1)
+          newState = {
+            ...state,
+            jobs: newJobs
+          }
+          break
+    default:
+      throw new Error(`Invalid action ${action.type}`)
+  }
 
-// 4. 
+  return newState
+}
+
 const App = () => {
-  //lan dau reducer chua goi voi, nhan gia tri khoi tao va return lai mot mang
-  //setCount day la dispatch thagn nay viet dung action nao se duoc thuc thi
-  const [count, dispatch] = useReducer(reducer, initState)
+  const [state, dispatch] = useReducer(reducer, initState)
+  const {job, jobs} = state
+  const jobIn = useRef()
+  console.log(jobs)
+  const handleSumbit = () => {
+    dispatch(addJob({job, jobIn}))
+    dispatch(setJob(""))
+    jobIn.current.focus()
+  }
+  
   return (
 
       <div>
-          <h1>{count}</h1>
-          <button onClick = {() => dispatch(DOWN_ACTION)}>
-            Down
+          <h1>To do</h1>
+          <input 
+            ref={jobIn}
+            value={job}
+            placeholder="Enter todo"
+            onChange={e => {
+              dispatch(setJob(e.target.value))
+            }}
+          />
+          <button onClick={handleSumbit}>
+            ADD
           </button>
-          <button onClick = {() => dispatch(UP_ACTION)}>
-            Up
-          </button>
+          <ul>
+            {jobs.map((c, i) => (
+              <li key={i}>{c} 
+                <span onClick={() => dispatch(deleteJob(i))}>&times;</span>
+              </li>
+            ))}
+          </ul>
       </div>
   )
 }
